7cf7e700fcabdb702231d12b7e307d8c093ab557
frankwon11 <frankwon11@gmail.com>
2024-03-20 23:12:20 +0900

    Initial Commit

---
+++ OurPack/OurPackApp.swift
@@ -0,0 +1,17 @@
+//
+//  OurPackApp.swift
+//  OurPack
+//
+//  Created by sseungwonnn on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct OurPackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
