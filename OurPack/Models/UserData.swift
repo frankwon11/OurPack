5f58099fc99d1d5644602457a65f0df2cc83366c
frankwon11 <frankwon11@gmail.com>
2024-03-21 20:51:49 +0900

    ✨ Feat: UserData 생성 (#3)

---
+++ OurPack/Models/UserData.swift
@@ -0,0 +1,13 @@
+//
+//  UserData.swift
+//  OurPack
+//
+//  Created by sseungwonnn on 3/21/24.
+//
+
+import SwiftUI
+
+struct UserData: Hashable {
+    var name: String
+    var color: Color
+}
---
+++ OurPack/data.swift
@@ -0,0 +1,11 @@
+//
+//  data.swift
+//  OurPack
+//
+//  Created by sseungwonnn on 3/21/24.
+//
+
+import SwiftUI
+
+let FRIENDS: [UserData] = [.init(name: "Sadie", color: .red), .init(name: "Mac", color: .green)]
+let USER: UserData = .init(name: "Shuwn", color: .blue)
